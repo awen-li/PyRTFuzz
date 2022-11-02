3cd7673acb49e769cae051160cde1e30fc97ddda
Daybreak2019 <li.wen@wsu.edu>
2022-11-02 12:32:39 -0700

    init for build.sh

---
+++ build.sh
@@ -0,0 +1,24 @@
+
+BASE_DIR=`pwd`
+
+# 1. build libfuzzer
+if [ ! -n "$LLVM_PATH" ]; then
+	echo "Plase set LLVM_PATH before building libfuzzer!"
+	exit 0
+fi
+
+cd $LLVM_PATH/llvm-11.0.0.src/projects/compiler-rt-11.0.0.src/lib/
+if [ ! -L fuzzer ]; then
+	if [ -d fuzzer ]; then
+		rm -rf fuzzer
+	fi
+	
+	ln -s $BASE_DIR/libfuzzer fuzzer
+fi
+
+cd $LLVM_PATH/build
+make -j4
+
+
+# 2. build atheris
+
