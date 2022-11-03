0ec0f99118772288b75c6148af7c8e1490afcdf8
Daybreak2019 <li.wen@wsu.edu>
2022-11-03 15:45:02 -0700

    debug done of static instrumentation

--- experiments/buildCPython.sh
+++ experiments/buildCPython.sh
@@ -4,17 +4,17 @@ BASE_DIR=`pwd`
 LD_CLANG="$(python -c "import sysconfig; print(sysconfig.get_config_var('LDSHARED'))")"	
 LD_CLANG=`echo $LD_CLANG | sed 's/^gcc/clang/'`
 
-export LDFLAGS="$(python -c "import atheris; print(atheris.path())")/asan_with_fuzzer.so"
+export LDFLAGS="$(python -c "import atheris; print(atheris.path())")/asan_with_fuzzer.so -pthread"
 
 export LDSHARED=$LD_CLANG
 export CC="clang" CFLAGS="-fsanitize=fuzzer-no-link" CXX="clang++" CXXFLAGS="-fsanitize=fuzzer-no-link"
-
+export ASAN_OPTIONS=detect_leaks=0
 
 if [ ! -d "cpython" ]; then
 	git clone https://github.com/Daybreak2019/cpython.git
-	
+	cd cpython && ./configure
 fi
 
-cd cpython && ./configure
+cd $BASE_DIR/cpython
 make 
 #make install
