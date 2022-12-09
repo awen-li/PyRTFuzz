df92c1f5940c5cd1c2687036349f56a4762f8081
Daybreak2019 <li.wen@wsu.edu>
2022-12-09 10:17:38 -0800

    initialize for APP gen

---
+++ apispec/PyGen/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="pygen",
+    version="1.0.0",
+    author="Wen Li",
+    author_email="li.wen@wsu.edu",
+    description="python APP generation",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Daybreak2019/CpyFuzz",
+    packages=['pygen']
+)
