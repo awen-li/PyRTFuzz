110b9a5b63e909040fb3893485c9c38262e0ec04
Daybreak2019 <li.wen@wsu.edu>
2022-12-17 20:19:24 -0800

    pprograph construction

--- apispec/PyGen/pygen/astop/propgraph.py
+++ apispec/PyGen/pygen/astop/propgraph.py
@@ -31,7 +31,7 @@ class graph:
 """
 
 class PgNode ():
-    def __init__ (self,Id, Type):
+    def __init__ (self, Id, Type):
         self.Id = Id
         self.InEdge = []
         self.OutEdge = []
@@ -49,8 +49,11 @@ class PropGraph (NodeVisitor):
     
     def __init__ (self, MainFunc='RunFuzz'):
         self.MainFunc = MainFunc
+        self.FuncList = {}
         self.Id2Node  = {}
         self.EdgeList = []
+
+        self.CurClass = None
 
     def AddNode (self, Type):
          Nd = PgNode (PropGraph.NodeId, Type) 
@@ -63,7 +66,41 @@ class PropGraph (NodeVisitor):
         self.EdgeList.append (Edge)
 
 
+    def VisitAst(self, node):
+        if node is None:
+            return
+
+        method = 'pg_' + node.__class__.__name__.lower()
+        print (method)
+        operator = getattr(self, method, self.generic_visit)        
+        return operator(node)
+
+    def pg_call(self, node):
+        print (ast.dump (node.func), end="\n\n")
+        if isinstance(node.func, Attribute):
+            self.visit (node.func)
+        else:
+            pass
+    
+    def pg_functiondef (self, node):
+        print (ast.dump (node), end="\n\n")
+        for st in node.body:
+            self.VisitAst (st)
+
+    def pg_classdef(self, node):
+        self.CurClass = node.name
+        
+        for st in node.body:
+            self.VisitAst (st)
+            
+        self.CurClass = None
+        
     def Build (self):
+        print ("\r\n================ PropGraph.Build ================\r\n")
         for tmpt in ATs.TmptList:
             print (tmpt)
+            astTmpt = ast.parse(tmpt)
+            print (ast.dump (astTmpt), end="\n\n")
+            
+            self.VisitAst (astTmpt.body[0])
 
