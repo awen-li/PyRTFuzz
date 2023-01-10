171e9500838a8773f9131e3e39aa11f8feb5b4b3
Daybreak2019 <li.wen@wsu.edu>
2023-01-10 13:48:32 -0800

    demo for retrieving functions' arg type

--- apispec/PySpec/DynSpec/pytrace/tracing.py
+++ apispec/PySpec/DynSpec/pytrace/tracing.py
@@ -43,6 +43,19 @@ class Tracing:
         print ("----> __exit__................, TracedStmts = ", self.TracedStmts)
         _unsettrace()
 
+    def GetValue(self, Frame, ValName):
+        if ValName in Frame.f_locals:
+            return Frame.f_locals[ValName]
+        elif ValName in Frame.f_globals:
+            return Frame.f_globals[ValName]
+        else:
+            Builtins = Frame.f_globals['__builtins__']
+            if isinstance(Builtins, dict) and (ValName in Builtins):
+                return Builtins[ValName]
+            elif isinstance(Builtins, Module) and hasattr(Builtins, ValName):
+                return getattr(Frame.f_globals['__builtins__'], ValName)
+        return None
+
     def Tracing(self, Frame, Event, Arg):
         if self.IsSingleCase and self.SetUp == False:
             Step = os.environ.get ("case_step")
@@ -55,9 +68,22 @@ class Tracing:
         self.CoName = Code.co_name
 
         _, ScriptName = os.path.split(Code.co_filename)
-        LineNo  = Frame.f_lineno      
+        LineNo  = Frame.f_lineno 
 
-        Msg = "[Python]:" + ScriptName + ": " +  str(LineNo) + " : " + Code.co_name
+        if Event == 'call':    
+            Msg = "[Python]:" + ScriptName + ": " +  str(LineNo) + " : calling " + Code.co_name
+            if Code.co_name == 'add':
+                x = self.GetValue (Frame, 'x')
+                print (type(x))
+
+                y = self.GetValue (Frame, 'y')
+                print (type(y))
+
+            if Code.co_name == 'show':
+                info = self.GetValue (Frame, 'info')
+                print (type(info))
+        else:
+            Msg = "[Python]:" + ScriptName + ": " +  str(LineNo) + " : " + Code.co_name + " -> EVENT = " + Event
         print (Msg)
         
         return self.Tracing
