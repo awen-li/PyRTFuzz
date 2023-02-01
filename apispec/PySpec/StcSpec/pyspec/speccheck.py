5715dc8a814b88de6881b66ad7525080a1371fec
Daybreak2019 <li.wen@wsu.edu>
2023-01-31 17:39:21 -0800

    add type statistic for type collection

--- apispec/PySpec/StcSpec/pyspec/speccheck.py
+++ apispec/PySpec/StcSpec/pyspec/speccheck.py
@@ -1,5 +1,6 @@
 
 import os
+import numpy as np
 from .apispec_load import *
 
 logFileName = "unrecognized_api_spec.txt"
@@ -9,11 +10,38 @@ class ApiSpecCheck ():
         self.PyLibs = self.InitPyLibs (ApiSpec)
         if os.path.exists (logFileName):
             os.remove (logFileName)
+        self.Types = {}
 
     def InitPyLibs (self, apiSpecXml):
         apiSpec = ApiSpecLoader (apiSpecXml)
         apiSpec.Parser ()
         return apiSpec.PyLibs
+
+    def UpdateType (self, Type):
+        num = self.Types.get (Type)
+        if num == None:
+            self.Types [Type] = 1
+        else:
+            self.Types [Type] = num + 1
+        
+    def CheckTypes (self, Api):
+        for arg in Api.Args:
+            type = arg.split (':')[1]
+            if type == 'None':
+                continue
+            self.UpdateType (type)
+            
+        for arg in Api.PosArgs:
+            type = arg.split (':')[1]
+            if type == 'None':
+                continue
+            self.UpdateType (type)
+    
+        for arg in Api.KwoArgs:
+            type = arg.split (':')[1]
+            if type == 'None':
+                continue
+            self.UpdateType (type)     
 
     def CheckApi (self, Api):
         # args
@@ -45,6 +73,18 @@ class ApiSpecCheck ():
             print ("[lib]%s, [module]%s, [class]%s, [api]Args:%s, PosArgs:%s, KwoArgs:%s, Ret:%s"\
                 %(libName, mdName, claName, str(api.Args), str(api.PosArgs), str(api.KwoArgs), str(api.Ret)), file=f)
 
+    def LogTypeInfo (self):
+        SortedTypes = sorted (self.Types.items (), key=lambda x:x[1], reverse=True)
+
+        print ("\n\n")
+        print ("##############       Type statistic     ####################\n")
+        Index = 1
+        for TypeName, Type in SortedTypes:
+            print ("[%-4d] Type: %-32s, Number: %-8s" %(Index, TypeName, Type))
+            Index += 1
+        print ("############################################################\n")
+            
+        
     def Check (self):
         TotalApiNum = 0
         TypeKnowns   = 0
@@ -54,6 +94,7 @@ class ApiSpecCheck ():
                 for clsName, cls in pyMoudle.Classes.items ():
                     TotalApiNum += len (cls.Apis)
                     for apiName, api in cls.Apis.items ():
+                        self.CheckTypes (api)
                         Typed = self.CheckApi (api)
                         if Typed == True:
                             TypeKnowns += 1
@@ -62,6 +103,7 @@ class ApiSpecCheck ():
 
                 TotalApiNum += len (pyMoudle.Apis)
                 for apiName, api in pyMoudle.Apis.items ():
+                    self.CheckTypes (api)
                     Typed = self.CheckApi (api)
                     if Typed == True:
                         TypeKnowns += 1
@@ -71,8 +113,8 @@ class ApiSpecCheck ():
         print ("\n#####################   ApiSpecCheck   #####################")
         print ("### TotalApiNum   = %d" %TotalApiNum)
         print ("### TypeKnownsNum = %d" %TypeKnowns)
-        print ("### Percentage = %f" %(TypeKnowns/TotalApiNum*1.0))
+        print ("### Percentage    = %f" %(TypeKnowns/TotalApiNum*1.0))
         print ("############################################################\n")
     
-    
+        self.LogTypeInfo ()
                 
