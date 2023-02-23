cce5069bcbe91e9ef9c55ae7b872ac0449dd763e
Daybreak2019 <li.wen@wsu.edu>
2023-02-23 12:15:01 -0800

    debugging: runnable apps reach 63%

--- test/pyfuzz_run_initial_seeds.py
+++ test/pyfuzz_run_initial_seeds.py
@@ -38,6 +38,20 @@ def RunProcess (Cmd):
         break
     return
 
+def GetTests (Path):
+    AllTests = []
+    ModDir = os.walk(Path)
+    TotalNum = 0
+    SuccessNum = 0
+    for Path, Dirs, Pys in ModDir:      
+        for py in Pys:
+            Mod, Ext = os.path.splitext(py)
+            if Ext != ".py":
+                continue
+            PyFile = os.path.join(Path, py)
+            AllTests.append (PyFile)
+    return AllTests
+
 InitFlag = '../experiments/seeds/initial_done'
 if not os.path.exists (InitFlag):
     SrvPort = random.randint(10000, 65531)
@@ -45,33 +59,27 @@ if not os.path.exists (InitFlag):
     atheris.GetInitialSeeds ('../experiments/seeds')
     atheris.Done ()
 
-ModDir = os.walk('../experiments/seeds')
+AllTests = GetTests ('../experiments/seeds')
 TotalNum = 0
 SuccessNum = 0
-for Path, Dirs, Pys in ModDir:      
-    for py in Pys:
-        Mod, Ext = os.path.splitext(py)
-        if Ext != ".py":
-            continue
-        PyFile = os.path.join(Path, py)
-        
-        Cmd = 'python -m runone ' + PyFile
-        sTimer = Timer(20, TimeOut)
-        sTimer.start()
-        
-        print ("[%d]Execute: %s" %(TotalNum, Cmd), end='')
-        RunProcess (Cmd)
-        TotalNum += 1
+for PyFile in AllTests:
+    Cmd = 'python -m runone ' + PyFile
+    sTimer = Timer(20, TimeOut)
+    sTimer.start()
+ 
+    RunProcess (Cmd)
+    TotalNum += 1
 
-        if RunResult == False:
-            print (' ----> Fail')
-        else:
-            print (' ----> Success')
-            SuccessNum += 1
-        
-        sTimer.cancel ()
+    sTimer.cancel ()
+    if RunResult == False:
+        print ("[%d]Execute: %s ----> Fail (%d/%.2f)" %(TotalNum, Cmd, SuccessNum, SuccessNum*1.0/TotalNum))
+    else:
+        SuccessNum += 1
+        print ("[%d]Execute: %s ----> Success (%d/%.2f)" %(TotalNum, Cmd, SuccessNum, SuccessNum*1.0/TotalNum))       
 
-print ("\n Success rate: [%d/%d]%.2f" %(SuccessNum, TotalNum, SuccessNum*1.0/TotalNum))
+print ("\n###Done, Success rate: [%d/%d]%.2f" %(SuccessNum, TotalNum, SuccessNum*1.0/TotalNum))
+sys.exit (0)
+
 
 
 
