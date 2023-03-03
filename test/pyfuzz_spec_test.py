a0efed47a5ea6b2e5137492869cab017f3db367e
Daybreak2019 <li.wen@wsu.edu>
2023-03-03 09:02:40 -0800

    debugging: update tests

--- test/pyfuzz_spec_test.py
+++ test/pyfuzz_spec_test.py
@@ -48,6 +48,11 @@ class ApiTest (SpecTest):
     
     def TestSet (self):
         self.AssertApi ("sqlite3.dbapi2.DateFromTicks", "ret = sqlite3.dbapi2.DateFromTicks(ticks)%%['int']", "['ticks:int']")
+        self.AssertApi ("distutils.version.StrictVersion.parse", "obj.parse(vstring)%%['str']", "['vstring:str']")
+        self.AssertApi ("distutils.core.run_setup", "distutils.core.run_setup(script_name,script_args,stop_after)%%['str', 'NoneType', 'str']", "['script_name:str', 'script_args:NoneType', 'stop_after:str']")
+        self.AssertApi ("encodings.gb18030.getregentry", "encodings.gb18030.getregentry()%%[]", "[]")
+        self.AssertApi ("email.iterators.typed_subpart_iterator", "email.iterators.typed_subpart_iterator(msg,maintype,subtype)%%['Message', 'str', 'str']", "['msg:Message', 'maintype:str', 'subtype:str']")
+        
 
 class ClsTest (SpecTest):
     def __init__ (self):
@@ -67,6 +72,10 @@ class ClsTest (SpecTest):
     
     def TestSet (self):
         self.AssertClass ('HTMLParser', "obj = html.parser.HTMLParser()%%[]", "_markupbase.ParserBase")
+        self.AssertClass ("Menubutton", "obj = tkinter.ttk.Menubutton()%%['NoneType']", "tkinter.ttk.Widget")
+        self.AssertClass ("LWPCookieJar", "obj = http.cookiejar.LWPCookieJar()%%['NoneType', 'NoneType', 'NoneType']", "http.cookiejar.FileCookieJar")
+        self.AssertClass ("HeaderParser", "obj = email.parser.HeaderParser()%%['NoneType']", "email.parser.Parser")
+        self.AssertClass ("RawTurtle", "obj = turtle.RawTurtle()%%['NoneType', 'NoneType', 'NoneType', 'NoneType']", "turtle.TPen")
 
 class MdTest (SpecTest):
     def __init__ (self):
@@ -86,6 +95,10 @@ class MdTest (SpecTest):
     
     def TestSet (self):
         self.AssertMoudle ("html.parser", "['html', 'html.parser', 're']", "['html:unescape']")
+        self.AssertMoudle ("email.policy", "['email', 'email.policy', 're', 'sys']", "['email._policybase:Compat32', 'email._policybase:Policy', 'email._policybase:compat32', 'email.contentmanager:raw_data_manager', 'email.headerregistry:HeaderRegistry', 'email.message:EmailMessage']")
+        self.AssertMoudle ("dbm.dumb", "['ast:_ast', 'collections.abc', 'dbm', 'dbm.dumb', 'io:_io', 'os:_os']", "[]")
+        self.AssertMoudle ("encodings.iso8859_14", "['codecs', 'encodings', 'encodings.iso8859_14']", "[]")
+        self.AssertMoudle ("pathlib", "['fnmatch', 'functools', 'grp', 'io', 'nt', 'ntpath', 'os', 'pathlib', 'posixpath', 'pwd', 're', 'sys']", "['errno:EBADF', 'errno:EINVAL', 'errno:ELOOP', 'errno:ENOENT', 'errno:ENOTDIR', 'operator:attrgetter', 'stat:S_ISBLK', 'stat:S_ISCHR', 'stat:S_ISDIR', 'stat:S_ISFIFO', 'stat:S_ISLNK', 'stat:S_ISREG', 'stat:S_ISSOCK', 'urllib.parse:quote_from_bytes']")
     
 
 if __name__ == "__main__":
