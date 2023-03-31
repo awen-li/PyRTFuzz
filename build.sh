96de904e5a652907b18c3c7e5da3510ba7025a75
Daybreak2019 <li.wen@wsu.edu>
2023-03-31 12:56:32 -0700

    add benchmarks of python 3.7/3.8

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 BASE_DIR=`pwd`
 INSTALL_PATH=/root/anaconda3
 PRIMARY_PYTHON=python3.9
+ALL_VERSIONS=("3.9.15" "3.8.15" "3.7.15")
 
 setPython ()
 {
@@ -37,35 +38,39 @@ cd $LLVM_PATH/build
 make -j4
 
 
-# 2. install python from source
-cd $BASE_DIR/cpython
 setPython $PRIMARY_PYTHON
 python --version
+for Ver in ${ALL_VERSIONS[@]}
+do
+	INSTALL_VER=`echo ${VERSION: 0: 3}`
 
-PYTHON_VER=Python-3.9.15
-if [ ! -d "$PYTHON_VER" ]; then
-	apt-get install openssl	
-	tar -xvf $PYTHON_VER.tar.xz
-	
-	cd $PYTHON_VER && ./configure --prefix=$INSTALL_PATH --enable-optimizations --with-openssl=/root/anaconda3
-	make clean && make && make altinstall
-fi
+	# 2. install python from source
+	cd $BASE_DIR/cpython
+	PYTHON_PATH="Python-$Ver"
+	if [ ! -d "$PYTHON_PATH" ]; then
+		apt-get install openssl	
+		tar -xvf $PYTHON_PATH.tar.xz
+		
+		cd $PYTHON_PATH && ./configure --prefix=$INSTALL_PATH --enable-optimizations --with-openssl=/root/anaconda3
+		make clean && make && make altinstall
+	fi
+	rm -rf $PYTHON_PATH
 
-# 3. build atheris
-setPython python3.9
-export ASAN_OPTIONS=detect_leaks=0
-cd $BASE_DIR/atheris
-if [ -d "build" ]; then
-	rm -rf build
-fi
-python setup.py install
-cd -
+	# 3. build atheris
+	setPython "python$INSTALL_VER"
+	export ASAN_OPTIONS=detect_leaks=0
+	cd $BASE_DIR/atheris
+	if [ -d "build" ]; then
+		rm -rf build
+	fi
+	python setup.py install
+	cd -
+
+	# 4. install fuzzwrapper
+	cd $BASE_DIR/fuzzwrapper && ./build.sh && cd -
 
 
-# 4. install fuzzwrapper
-cd $BASE_DIR/fuzzwrapper && ./build.sh && cd -
+	# 5. install apispec toolkit
+	cd $BASE_DIR/apispec && ./build.sh && cd -
+done
 
-
-
-# 5. install apispec toolkit
-cd $BASE_DIR/apispec && ./build.sh && cd -
--- experiments/buildCPython.sh
+++ experiments/buildCPython.sh
@@ -1,7 +1,7 @@
 
 BASE_DIR=`pwd`
 INSTALL_PATH=/root/anaconda3
-PYTHON_VER=Python-3.9.15
+ALL_VERSIONS=("3.9.15" "3.8.15" "3.7.15")
 
 setPython ()
 {
@@ -15,28 +15,47 @@ setPython ()
 	cd -
 }
 
-setPython python3.9
-export ASAN_OPTIONS=detect_leaks=0
-
-LD_CLANG="$(python -c "import sysconfig; print(sysconfig.get_config_var('LDSHARED'))")"	
-LD_CLANG=`echo $LD_CLANG | sed 's/^gcc/clang/'`
-
-export LDFLAGS="$(python -c "import atheris; print(atheris.path())")/asan_with_fuzzer.so -pthread"
-export LDSHARED=$LD_CLANG
-export CC="clang" CFLAGS="-fsanitize=fuzzer-no-link" CXX="clang++" CXXFLAGS="-fsanitize=fuzzer-no-link"
-
-if [ ! -d "$PYTHON_VER" ]; then
-	cp ../cpython/$PYTHON_VER.tar.xz ./
-	tar -xvf $PYTHON_VER.tar.xz
-	cd $PYTHON_VER && ./configure --prefix=$INSTALL_PATH --enable-optimizations --with-openssl=/root/anaconda3
+function InstallPython ()
+{
+	VERSION=$1
+	PYTHON_PATH="Python-"$VERSION
+	INSTALL_VER=`echo ${VERSION: 0: 3}`
 	
-	make clean && make
-	make altinstall
-	cd -
+	echo "InstallPython --> $VERSION, $PYTHON_PATH, $INSTALL_VER"
+	return
 
-	#link to the current python
-	setPython python3.9	
-fi
+	setPython python$INSTALL_VER
+	export ASAN_OPTIONS=detect_leaks=0
+
+	LD_CLANG="$(python -c "import sysconfig; print(sysconfig.get_config_var('LDSHARED'))")"	
+	LD_CLANG=`echo $LD_CLANG | sed 's/^gcc/clang/'`
+
+	export LDFLAGS="$(python -c "import atheris; print(atheris.path())")/asan_with_fuzzer.so -pthread"
+	export LDSHARED=$LD_CLANG
+	export CC="clang" CFLAGS="-fsanitize=fuzzer-no-link" CXX="clang++" CXXFLAGS="-fsanitize=fuzzer-no-link"
+
+	if [ ! -d "$PYTHON_PATH" ]; then
+		cp ../cpython/$PYTHON_PATH.tar.xz ./
+		tar -xvf $PYTHON_PATH.tar.xz
+		cd $PYTHON_PATH && ./configure --prefix=$INSTALL_PATH --enable-optimizations --with-openssl=/root/anaconda3
+		
+		make clean && make
+		make altinstall
+		cd -
+
+		#link to the current python
+		setPython python$INSTALL_VER
+	fi
+}
+
+
+# Instrument all pythons
+for Ver in ${ALL_VERSIONS[@]}
+do
+	InstallPython $Ver
+done
+
+
 
 # link apispec here
 SpecFile=`python -c "from pyspec import ApiSpecGen; print(ApiSpecGen.GetSpecName())"`
