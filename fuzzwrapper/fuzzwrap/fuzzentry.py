849aa1825775e60acf4d532f683dd72d1ba89e13
Daybreak2019 <li.wen@wsu.edu>
2023-04-11 08:09:52 -0700

    update time budget to 90 s

--- fuzzwrapper/fuzzwrap/fuzzentry.py
+++ fuzzwrapper/fuzzwrap/fuzzentry.py
@@ -23,7 +23,7 @@ def _ArgProc ():
     
     # default time budget
     if Index == len (sys.argv):
-       sys.argv.append ("-max_total_time=180")
+       sys.argv.append ("-max_total_time=90")
 
     # default memory budget
     sys.argv.append ("-rss_limit_mb=8192")
