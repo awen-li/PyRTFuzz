27045e2d7aa61d47f1ee0e46304e670ba4d6c73b
Daybreak2019 <li.wen@wsu.edu>
2023-04-21 11:22:53 -0700

    add drawing for RQ3.2

--- tool/Draw/draw.sh
+++ tool/Draw/draw.sh
@@ -2,3 +2,6 @@
 # RQ1
 python picDraw.py -q rq1
 
+
+# RQ3.1
+python picDraw.py -q rq3.1
--- tool/Draw/picDraw.py
+++ tool/Draw/picDraw.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 
-import csv
+import re
 import os
 import sys, getopt
 import numpy as np 
@@ -33,6 +33,26 @@ def LoadInput (File='PRTFuzz_perf.log'):
             TpNodeList.append (TpNode (Items[0], Items[1], Items[2]))
     return TpNodeList
 
+def GetAllLogs (DirName):
+    LogFiles = []
+
+    DirList = []
+    DirList.append (DirName)
+
+    while len (DirList) != 0:
+        DirName = DirList.pop (0)
+        FileList = os.listdir (DirName)
+        for file in FileList:
+            Path = os.path.join (DirName, file)
+            if os.path.isdir (Path):
+                DirList.append (Path)
+                continue
+
+            if Path.find ("PRTFuzz_perf.log") != -1:
+                LogFiles.append (Path)
+
+    return LogFiles
+
 def GetCovByTimeLine (TpNodeList, TimeLine):
     CovList = {}
     for TN in TpNodeList:
@@ -57,11 +77,8 @@ def GetAppNumByTimeLine (TpNodeList, TimeLine):
         AnList.append (AnList[-1])
     return AnList
 
-def GetPicName (File):
-    Index = File.rfind ('.')
-    return File[0:Index]
-
-def DrawRQ1 (File):
+def DrawRQ1 (Dir):
+    File = GetAllLogs (Dir)[0]
     TpNodeList = LoadInput (File)
 
     TimeLine = [v*4 for v in range (0, 19)]
@@ -73,7 +90,7 @@ def DrawRQ1 (File):
     X = TimeLine
     Y = GetCovByTimeLine (TpNodeList, TimeLine)
     print (Y)
-    axCov.plot(X, Y, linewidth=2.0, color='red')
+    axCov.plot(X, Y, linewidth=2.0, color='black')
     #axCov.set_title('Block coverage over time', fontsize=10)
     axCov.grid(color = 'grey', linestyle = '--', linewidth = 0.5)
     axCov.set(ylabel="#Block-coverage")
@@ -82,7 +99,7 @@ def DrawRQ1 (File):
     # AppNum over time
     Y = GetAppNumByTimeLine (TpNodeList, TimeLine)
     print (Y)
-    axApp.plot(X, Y, linewidth=2.0, color='blue')
+    axApp.plot(X, Y, linewidth=2.0, color='black')
     #axApp.set_title('Application number over time', fontsize=10)
     axApp.grid(color = 'grey', linestyle = '--', linewidth = 0.5)
     axApp.set(ylabel="#Application")
@@ -91,11 +108,91 @@ def DrawRQ1 (File):
     fig.set_figwidth(12)
     fig.set_figheight(4)
     
-    PicName = GetPicName (File)
-    plt.savefig(PicName)
+    plt.savefig(Dir+'/PIC_RQ1')
     plt.close()
-    
     return
+
+def DrawRQ3_1 (Dir):
+    TimeLine = [v*4 for v in range (0, 19)]
+    fig, (axCov, axApp) = plt.subplots(1, 2)
+
+    LineType = {'1':'-', '4':'--', '16':'-.', '64':':', '128':'-', '256':'--', '512':'-.', '1024':':'}
+    ColorType = {'1':'k', '4':'k', '16':'k', '64':'k', '128':'g', '256':'g', '512':'g', '1024':'g'}
+
+    AllLogs = GetAllLogs (Dir)
+    for Log in AllLogs:
+        Complexity = re.findall(r"-complex-(\d+?)/", Log)[0]
+        LT = LineType[Complexity]
+        CL = ColorType[Complexity]
+
+        TpNodeList = LoadInput (Log)
+        
+        # Cov over time
+        X = TimeLine
+        Y = GetCovByTimeLine (TpNodeList, TimeLine)
+        print (Y)
+        axCov.plot(X, Y, linewidth=2.0, color=CL, linestyle=LT)
+        #axCov.set_title('Block coverage over time', fontsize=10)
+        axCov.grid(color = 'grey', linestyle = '--', linewidth = 0.5)
+        axCov.set(ylabel="#Block-coverage")
+        axCov.set(xlabel="Time (hour)")
+
+        # AppNum over time
+        Y = GetAppNumByTimeLine (TpNodeList, TimeLine)
+        print (Y)
+        axApp.plot(X, Y, linewidth=2.0, color=CL, linestyle=LT)
+        #axApp.set_title('Application number over time', fontsize=10)
+        axApp.grid(color = 'grey', linestyle = '--', linewidth = 0.5)
+        axApp.set(ylabel="#Application")
+        axApp.set(xlabel="Time (hour)")
+
+    fig.set_figwidth(12)
+    fig.set_figheight(4)
+    
+    plt.savefig(Dir+'/PIC_RQ3-1_Complexity')
+    plt.close()
+
+def DrawRQ3_2 (Dir):
+    TimeLine = [v*4 for v in range (0, 19)]
+    fig, (axCov, axApp) = plt.subplots(1, 2)
+
+    LineType = {'1':'-', '30':'--', '60':'-.', '90':':', '180':'-', '360':'--'}
+    ColorType = {'1':'k', '30':'k', '60':'k', '90':'g', '180':'g', '360':'g'}
+
+    AllLogs = GetAllLogs (Dir)
+    for Log in AllLogs:
+        print (Log)
+        Complexity = re.findall(r"-Budget-(\d+?)/", Log)[0]
+        LT = LineType[Complexity]
+        CL = ColorType[Complexity]
+
+        TpNodeList = LoadInput (Log)
+        
+        # Cov over time
+        X = TimeLine
+        Y = GetCovByTimeLine (TpNodeList, TimeLine)
+        print (Y)
+        axCov.plot(X, Y, linewidth=2.0, color=CL, linestyle=LT)
+        #axCov.set_title('Block coverage over time', fontsize=10)
+        axCov.grid(color = 'grey', linestyle = '--', linewidth = 0.5)
+        axCov.set(ylabel="#Block-coverage")
+        axCov.set(xlabel="Time (hour)")
+
+        # AppNum over time
+        Y = GetAppNumByTimeLine (TpNodeList, TimeLine)
+        print (Y)
+        axApp.plot(X, Y, linewidth=2.0, color=CL, linestyle=LT)
+        #axApp.set_title('Application number over time', fontsize=10)
+        axApp.grid(color = 'grey', linestyle = '--', linewidth = 0.5)
+        axApp.set(ylabel="#Application")
+        axApp.set(xlabel="Time (hour)")
+
+    fig.set_figwidth(12)
+    fig.set_figheight(4)
+    
+    plt.savefig(Dir+'/PIC_RQ3-2_Lv2Budget')
+    plt.close()
+
 
 def main(argv):
     Rq = ''
@@ -113,8 +210,14 @@ def main(argv):
         print ("Rq = ", Rq, ", input invalid!!!!")
         return
     
-    if Rq == 'rq1':    
-        DrawRQ1 ('RQ1/GPU-cailab-FuzzResult-cpyfuzz-python3.9-48-covapp/FuzzResult/PRTFuzz_perf.log')
+    if Rq == 'rq1' or Rq == 'all':    
+        DrawRQ1 ('RQ1')
+
+    if Rq == 'rq3.1' or Rq == 'all':    
+        DrawRQ3_1 ('RQ3.1')
+
+    if Rq == 'rq3.2' or Rq == 'all':    
+        DrawRQ3_2 ('RQ3.2')
 
 if __name__ == "__main__":
     main(sys.argv[1:])
